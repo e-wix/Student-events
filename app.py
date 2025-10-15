3e18c21529dd3a8b665bdf94e5ce6e012ac9ecd3
e-wix <154426493+e-wix@users.noreply.github.com>
2025-10-15 09:39:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"  # Needed for flash messages
-DB_NAME = "events.json"
 SQL_DB = "events.db"
 
-# ---------- JSON Functions ----------
-def load_events():
-    if not os.path.exists(DB_NAME):
-        return {}
-    with open(DB_NAME) as f:
-        return json.load(f)
-
-def save_events(events):
-    with open(DB_NAME, "w") as f:
-        json.dump(events, f, indent=4)
 
 # ---------- SQL Setup ----------
 def init_sql_db():
@@ -77,11 +66,10 @@ events[event_id] = {
     "password": password  # stored in JSON
 }
 
-
-    save_events(events)
-    update_sql(events)
-    flash("Event created successfully!", "success")
-    return redirect("/")
+save_events(events)
+update_sql(events)
+flash("Event created successfully!", "success")
+return redirect("/")
 
 stored_password_hash = events[event_id].get("password", "")
 
@@ -117,9 +105,7 @@ if stored_password_hash:
     return redirect("/")
 
 # ---------- Download Routes ----------
-@app.route("/download-json")
-def download_json():
-    return send_file(DB_NAME, as_attachment=True)
+
 
 @app.route("/download-db")
 def download_db():
